2a88c8018d11a489aad5dc202a3af1d29084e320
Ruchi Mandve <ruchi.mandve@morningstar.com>
2021-09-14 21:09:10 +0530

    [ruchi]: implemented CalculateFare for multiplr rides in Cab invoice generator

--- src/test/java/CabInvoiceGeneratorTest.java
+++ src/test/java/CabInvoiceGeneratorTest.java
@@ -11,4 +11,24 @@ public class CabInvoiceGeneratorTest {
         double fare=cabinvoice.calculateFare(distance,time);
         Assert.assertEquals(25,fare,0.0);
     }
+
+    @Test
+    public void givenLessDistanceOrTime_ShouldReturnMinFare() {
+        CabInvoice cabinvoice=new CabInvoice();
+        double distance=0.1;
+        int time=1;
+        double fare=cabinvoice.calculateFare(distance,time);
+        Assert.assertEquals(5,fare,0.0);
+    }
+
+    @Test
+    public void givenMultipleRides_ShouldReturnInvoiceSummary() {
+        CabInvoice cabinvoice=new CabInvoice();
+//        double distance=0.1;
+//        int time=1;
+        Rides[] ride={new Rides(2.0,5),new Rides(0.1,1)
+        };
+        double fare=cabinvoice.calculateFare(ride);
+        Assert.assertEquals(30,fare,0.0);
+    }
 }
